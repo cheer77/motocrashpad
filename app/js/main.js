5c52fe6e51334ae119c7bbbde28badf872a8f97d
Aleksandr <sheph@ukr.net>
2021-11-11 16:26:58 +0200

    added soft scroll and fixed semantic errors

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,20 @@ $(function () {
         $('body').addClass('body_visible');
     }, 200);
 
+    $(".footer__links a").on("click", function (e) {
+        //отменяем стандартную обработку нажатия по ссылке
+        e.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
 
 
 });
